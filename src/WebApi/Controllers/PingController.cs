093dab3315cf9b898db72a31ec4de99a5cf4e871
Michal Gajda <michal@gajda.co.uk>
2025-02-16 21:03:58 +0000

    Code Cleanup

--- src/WebApi/Controllers/PingController.cs
+++ src/WebApi/Controllers/PingController.cs
@@ -3,11 +3,7 @@ namespace Cuculus.WebApi.Controllers;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController, Route("[controller]")]
-public class PingController : ControllerBase
+public sealed class PingController : ControllerBase
 {
-    [HttpGet(Name = "ping")]
-    public string Get()
-    {
-        return "Pong";
-    }
+    [HttpGet(Name = "ping")] public string Get() => "Pong";
 }
